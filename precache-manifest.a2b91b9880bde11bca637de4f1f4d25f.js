502cb15b893e7340bdaee6718dc8827739870920
josh <srijandasbiswas7540@gmail.com>
2020-03-09 18:35:22 +0530

    Updates

---
+++ precache-manifest.a2b91b9880bde11bca637de4f1f4d25f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12f8df6ac37e928cfd7a",
+    "url": "/react-todoapp/static/css/main.1e8fb3cc.chunk.css"
+  },
+  {
+    "revision": "12f8df6ac37e928cfd7a",
+    "url": "/react-todoapp/static/js/main.12f8df6a.chunk.js"
+  },
+  {
+    "revision": "cdd973e864aa128423b9",
+    "url": "/react-todoapp/static/js/1.cdd973e8.chunk.js"
+  },
+  {
+    "revision": "b1f04d793cf16cdaa992",
+    "url": "/react-todoapp/static/js/runtime~main.b1f04d79.js"
+  },
+  {
+    "revision": "943c31b39fa360538ac7e19302f86dd8",
+    "url": "/react-todoapp/index.html"
+  }
+];
